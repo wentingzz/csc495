aeda58b20199af1f4e42d002d81973e070226167
rjwoods <35344558+rjwoods@users.noreply.github.com>
2018-02-16 20:13:46 -0500

    Added function that prints the current game board
    So I added a method to the Game class called
    
         def printBoard(self, player):
    
    That, given the player whose turn is active, will print the game board (each zone with the top and bottom card of each zone, and the middle deck and remaining card numbers), the player's current hand, and the cards remaining in each player's hand. Basically, this will be called each turn.

--- p2.py
+++ p2.py
@@ -9,6 +9,8 @@ class Card:
         # extra color field because king's corner doesn't care about suit specifically (b or r)
         self.color = color
 
+
+
         # card value is based on the number, but is not always displayed as the number
         if val == 1:
             self.valSym = 'A'
@@ -22,10 +24,11 @@ class Card:
             self.valSym = str(val)
 
     def __repr__(self):
-        print("Card value: " + str(self.val))
-        print("Card suit: " + self.suit)
-        print("Card color: " + self.color)
-        print()
+        if self.val != 0:
+            card = self.valSym + ' of ' + self.suit + 's'
+            return card
+        else:
+            return 'NONE'
 
     # more specified card string representation for game board presentation
     def getCard(self):
@@ -102,6 +105,21 @@ class Board:
         self.Ebot = None
         self.Wbot = None"""
     def initfields(self):
+        # we need some sort of placeholder card for empty stacks
+        empty = Card(0, 'empty', 'empty')
+
+        self.C1.top = empty
+        self.C1.bot = empty
+
+        self.C2.top = empty
+        self.C2.bot = empty
+
+        self.C3.top = empty
+        self.C3.bot = empty
+
+        self.C4.top = empty
+        self.C4.bot = empty
+
         self.N.top = self.deck.pop()
         self.N.bot = self.N.top
 
@@ -114,7 +132,6 @@ class Board:
         self.W.top = self.deck.pop()
         self.W.bot = self.W.top
 
-
 class Game:
     # A game has a board, players p1-p4, and numplayers
     # p1 and p2 are required, with option p3 and p4
@@ -124,6 +141,7 @@ class Game:
         self.p2 = p2
         self.numplayers = 2
         self.p3 = p3
+        self.p4 = p4
         if p3 != 0:
             self.numplayers = 3
         if p4 != 0:
@@ -135,26 +153,89 @@ class Game:
     # other player's hands
     def printBoard(self, player):
 
+        """Here we have the game ready to go without specific rules
+            game.board = the playing field
+            game.board.N,S,E,W,C1,C2,C3,C4 for each field
+            game.board.N.top = the current card on the field you can play a card ontop of, 0 = blank
+            game.board.N.bot = the bottom card of a stack for moving how stacks
+            game.p1,p2,p3,p4 for players
+            """
 
+
+        #print the board
+        print('*---------------------------*---------------------------*---------------------------*')
+        print('|                           |                           |                           |')
+        print('| Top: %16s     | Top: %16s     | Top: %16s     |' % (self.board.C1.top, self.board.N.top, self.board.C2.top,))
+        print('|                           |                           |                           |')
+        print('|            ****           |            ***            |            ****           |')
+        print('|            *C1*           |            *N*            |            *C2*           |')
+        print('|            ****           |            ***            |            ****           |')
+        print('|                           |                           |                           |')
+        print('| Bottom: %13s     | Bottom: %13s     | Bottom: %13s     |' % (self.board.C1.bot, self.board.N.bot, self.board.C2.bot,))
+        print('|                           |                           |                           |')
+        print('*---------------------------*---------------------------*---------------------------*')
+        print('|                           |                           |                           |')
+        print('| Top: %16s     |                           | Top: %16s     |' % ( self.board.W.top, self.board.E.top, ))
+        print('|                           |                           |                           |')
+        print('|            ***            |                           |            ***            |')
+        print('|            *W*            |           DECK            |            *E*            |')
+        print('|            ***            |          %2d CARDS         |            ***            |' % len(self.board.deck))
+        print('|                           |                           |                           |')
+        print('| Bottom: %13s     |                           | Bottom: %13s     |' % (self.board.W.bot, self.board.E.bot))
+        print('|                           |                           |                           |')
+        print('*---------------------------*---------------------------*---------------------------*')
+        print('|                           |                           |                           |')
+        print('| Top: %16s     | Top: %16s     | Top: %16s     |' % (self.board.C3.top,self.board.S.top,  self.board.C4.top))
+        print('|                           |                           |                           |')
+        print('|            ****           |            ***            |            ****           |')
+        print('|            *C3*           |            *S*            |            *C4*           |')
+        print('|            ****           |            ***            |            ****           |')
+        print('|                           |                           |                           |')
+        print('| Bottom: %13s     | Bottom: %13s     | Bottom: %13s     |' % (self.board.C3.bot,  self.board.S.bot, self.board.C4.bot))
+        print('|                           |                           |                           |')
+        print('*---------------------------*---------------------------*---------------------------*')
+        print()
 
         #player's hand
         print("Player's Hand (%d Cards):" % (len(player.hand)))
         for c in player.hand:
             print(c.getCard(), end='')
             print(', ', end='')
+        print('')
+        print('')
+
+        #player hand numbers
+        print('Number of Cards in Each Player\'s Hand:')
+        print(self.p1.name + ': ' + str(len(self.p1.hand)))
+        print(self.p2.name + ': ' + str(len(self.p2.hand)))
+        if self.numplayers >= 3:
+            print(self.p3.name + ': ' + str(len(self.p3.hand)))
+        if self.numplayers >= 4:
+            print(self.p4.name + ': ' + str(len(self.p4.hand)))
+        print('')
 
 
+
+
+
+"""Here we have the game ready to go without specific rules
+    game.board = the playing field
+    game.board.N,S,E,W,C1,C2,C3,C4 for each field
+    game.board.N.top = the current card on the field you can play a card ontop of, 0 = blank
+    game.board.N.bot = the bottom card of a stack for moving how stacks
+    game.p1,p2,p3,p4 for players 
+    """
 
 def initdeck(d):
     # makes each card in a deck in unshuffled order
     for i in range(1, 14):
-        d.append(Card(i, 'heart', 'red'))
+        d.append(Card(i, 'Heart', 'Red'))
     for i in range(1, 14):
-        d.append(Card(i, 'diamond', 'red'))
+        d.append(Card(i, 'Diamond', 'Red'))
     for i in range(1, 14):
-        d.append(Card(i, 'club', 'black'))
+        d.append(Card(i, 'Club', 'Black'))
     for i in range(1, 14):
-        d.append(Card(i, 'spade', 'black'))
+        d.append(Card(i, 'Spade', 'Black'))
     return 52
 
 
@@ -213,6 +294,11 @@ def kingscorner():
     # draw 7 cards each
     game.p1.newhand(deck)
     game.p2.newhand(deck)
+    if numplayers >= 3:
+        game.p3.newhand(deck)
+    if numplayers >= 4:
+        game.p4.newhand(deck)
+
     # play 1 card in each N,S,E,W
     game.board.initfields()
 
